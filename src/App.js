b0a8ac5c040d01d6d9eb2033c17e240ad7f821f6
MrFarhan <muhammadfarhan012345@gmail.com>
2020-06-14 19:16:30 +0500

    basic react app - assignment 01

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './styles.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+     <h2>Hello from <strong>Farhan</strong></h2>
+     <label><input type="text"/>This is an input field</label>
+  <p>This is a paragraph</p>
+<span>This is an image tag</span><img height="50px"src={"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"}/> 
+  
+<h4>Unorderd list seems like this </h4>
+     <ul>
+       <li>list item 1</li>
+       <li>list item 2</li>
+       <li>list item 3</li>
+       <ol>
+         <li>This is orderd list item 1</li>
+         <li>This is orderd list item 2</li>
+         <li>This is orderd list item 3</li>
+       
+       </ol>
+     </ul>
+     <span>Copy right @MrFarhan</span>
     </div>
   );
 }
+
 
 export default App;
